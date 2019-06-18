ed9292c23ccd907e2bd9cbd6aab1fd4df7766b13
jgalans <galansjavier@gmail.com>
2019-06-18 21:26:30 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -11,3 +11,16 @@ sleep 5
 sudo docker exec -it gnmicli ./bin/gnmi_get -xpath_target COUNTERS_DB -xpath interface:Ethernet1/in-pkts -target_addr 192.18.0.12:8080 -insecure -logtostderr
 
 sudo docker exec -it gnmicli ./bin/gnmi_get -xpath_target COUNTERS_DB -xpath interface:Ethernet1/out-pkts -target_addr 192.18.0.12:8080 -insecure -logtostderr
+
+#Desactivamos la interfaz Ethernet0
+sudo docker exec -it host1 ifconfig eth1 up
+
+#Ping desde host1 a host2 
+sudo docker exec -it host1 ping 192.168.2.2 -c5
+
+sleep 5
+
+#Monitorización del Switch2:
+sudo docker exec -it gnmicli ./bin/gnmi_get -xpath_target COUNTERS_DB -xpath interface:Ethernet1/in-pkts -target_addr 192.18.0.12:8080 -insecure -logtostderr
+
+sudo docker exec -it gnmicli ./bin/gnmi_get -xpath_target COUNTERS_DB -xpath interface:Ethernet1/out-pkts -target_addr 192.18.0.12:8080 -insecure -logtostderr
