6d7576fc0e53653d2ee3a7d489f4c37a9d479c25
jgalans <galansjavier@gmail.com>
2019-06-18 06:50:43 +0200

    Update test_telemetry.sh

--- test_telemetry.sh
+++ test_telemetry.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
 
-#Monitorización del Switch2:
-sudo docker exec -it gnmicli ./bin/gnmi_get -xpath_target COUNTERS_DB -xpath interface:Ethernet1/in-pkts -target_addr 192.18.0.12:8080 -insecure -logtostderr
-
 #Ping desde host1 a host2 
 sudo docker exec -it host1 ping 192.168.2.2 -c10
-
-sleep 5
-
-#Monitorización del Switch2:
-sudo docker exec -it gnmicli ./bin/gnmi_get -xpath_target COUNTERS_DB -xpath interface:Ethernet1/in-pkts -target_addr 192.18.0.12:8080 -insecure -logtostderr
