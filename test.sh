446058b88feac560421d0c0cbc3a8f35e0fadd5b
jgalans <galansjavier@gmail.com>
2019-06-18 23:14:01 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+sudo docker exec -it host2 ping 192.168.2.2 -c5 
+
 #Ping desde host1 a host2 
 sudo docker exec -it host1 ping 192.168.2.2 -c5 
 
@@ -7,4 +10,4 @@ sleep 5
 #Monitorización del Switch2:
 sudo docker exec -it gnmicli ./bin/gnmi_get -xpath_target COUNTERS_DB -xpath interface:Ethernet1/in-pkts -target_addr 192.18.0.12:8080 -insecure -logtostderr
 sudo docker exec -it gnmicli ./bin/gnmi_get -xpath_target COUNTERS_DB -xpath interface:Ethernet1/in-octets -target_addr 192.18.0.12:8080 -insecure -logtostderr
-sudo docker exec -it gnmicli ./bin/gnmi_get -xpath_target COUNTERS_DB -xpath interface:Ethernet1/in-errors -target_addr 192.18.0.12:8080 -insecure -logtostderr
+sudo docker exec -it gnmicli ./bin/gnmi_get -xpath_target COUNTERS_DB -xpath interface:Ethernet1/errors -target_addr 192.18.0.12:8080 -insecure -logtostderr
