c84a36976fad2cbc26f03820525b0b185c386cf7
jgalans <galansjavier@gmail.com>
2019-06-19 03:03:35 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,11 +2,14 @@
 #Ping desde host1 a switch1
 sudo docker exec -it host1 ping 192.168.1.1 -c10
 
-#Ping desde host1 a switch2
-sudo docker exec -it host1 ping 192.168.2.1 -c10
+#Ping desde switch1 a host1
+sudo docker exec -it switch1 ping 192.168.1.2 -c10
 
-#Ping desde host1 a host2 
-sudo docker exec -it host2 ping 192.168.1.2 -c10
+#Ping desde host2 a switch2
+sudo docker exec -it host2 ping 192.168.2.1 -c10
+
+#Ping desde switch2 a host2
+sudo docker exec -it switch2 ping 192.168.2.2 -c10
 
 sleep 20 
 
