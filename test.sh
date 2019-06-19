fca68cb2b5a9934681f151766234f67ff58e6aba
jgalans <galansjavier@gmail.com>
2019-06-19 02:25:41 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,12 +2,16 @@
 #Ping desde switch1 a host1
 sudo docker exec -it switch1 ping 192.168.1.2 -c 2 -l 32
 
-#Ping desde host1 a host2 
-sudo docker exec -it host1 ping 192.168.2.2 -c 2 -l 32
-
 #Ping desde host2 a host1 
 sudo docker exec -it host2 ping 192.168.1.2 -c 2 -l 32
 
+#Monitorización switch2
+sudo docker exec -it gnmicli ./bin/gnmi_get -xpath_target COUNTERS_DB -xpath interface:Ethernet1/in-pkts -target_addr 192.18.0.11:8080 -insecure -logtostderr
+sudo docker exec -it gnmicli ./bin/gnmi_get -xpath_target COUNTERS_DB -xpath interface:Ethernet1/out-pkts -target_addr 192.18.0.11:8080 -insecure -logtostderr
+sudo docker exec -it gnmicli ./bin/gnmi_get -xpath_target COUNTERS_DB -xpath interface:Ethernet0/in-pkts -target_addr 192.18.0.11:8080 -insecure -logtostderr
+sudo docker exec -it gnmicli ./bin/gnmi_get -xpath_target COUNTERS_DB -xpath interface:Ethernet0/out-pkts -target_addr 192.18.0.11:8080 -insecure -logtostderr
+
+#Monitorización switch2
 sudo docker exec -it gnmicli ./bin/gnmi_get -xpath_target COUNTERS_DB -xpath interface:Ethernet1/in-pkts -target_addr 192.18.0.12:8080 -insecure -logtostderr
 sudo docker exec -it gnmicli ./bin/gnmi_get -xpath_target COUNTERS_DB -xpath interface:Ethernet1/out-pkts -target_addr 192.18.0.12:8080 -insecure -logtostderr
 sudo docker exec -it gnmicli ./bin/gnmi_get -xpath_target COUNTERS_DB -xpath interface:Ethernet0/in-pkts -target_addr 192.18.0.12:8080 -insecure -logtostderr
