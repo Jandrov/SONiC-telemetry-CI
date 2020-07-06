9f11a04efff92998f16d3f992b752faf8c9d254f
jgalans <galansjavier@gmail.com>
2020-07-06 17:50:48 +0200

    Updating test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#Ping desde host1 a host2
 sudo docker exec -it host1 ping 192.168.2.2 -c10
 
 #Ping desde host1 a switch1
@@ -7,6 +8,9 @@ sudo docker exec -it host1 ping 192.168.1.1 -c50
 
 #Ping desde switch1 a host1
 sudo docker exec -it switch1 ping 192.168.1.2 -c50
+
+#Ping desde host2 a host1
+sudo docker exec -it host2 ping 192.168.1.2 -c10
 
 #Ping desde host2 a switch2
 sudo docker exec -it host2 ping 192.168.2.1 -c50
@@ -16,7 +20,7 @@ sudo docker exec -it switch2 ping 192.168.2.2 -c50
 
 sleep 20 
 
-#Monitorización switch2
+#Monitorización switch1
 sudo echo "Paquetes recibidos por Ethernet 1 en switch1"
 sudo docker exec -it gnmicli ./bin/gnmi_get -xpath_target COUNTERS_DB -xpath interface:Ethernet1/in-pkts -target_addr 192.18.0.11:8080 -insecure -logtostderr
 sudo echo "Paquetes recibidos por Ethernet 0 en switch1"
