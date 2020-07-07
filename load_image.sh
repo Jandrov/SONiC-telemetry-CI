b5a8c1ca5fb1164036b86f826ba0d5e2b0f3ace1
jgalans <galansjavier@gmail.com>
2020-07-07 13:10:32 +0200

    updating docker sonic version

--- load_image.sh
+++ load_image.sh
@@ -1,6 +1,7 @@
-wget https://sonic-jenkins.westus2.cloudapp.azure.com/job/p4/job/buildimage-p4-all/613/artifact/target/docker-sonic-p4.gz
+#wget https://sonic-jenkins.westus2.cloudapp.azure.com/job/p4/job/buildimage-p4-all/543/artifact/target/docker-sonic-p4.gz
+wget https://sonic-jenkins.westus2.cloudapp.azure.com/job/p4/job/buildimage-p4-all/lastStableBuild/artifact/target/docker-sonic-p4.gz
+
 sudo docker load < docker-sonic-p4.gz
-sudo docker pull ubuntu:14.04
 
 #Contruimos las imagenes atendiendo al Dockerfile
 sudo docker build -f Dockerfile_sonic . 
