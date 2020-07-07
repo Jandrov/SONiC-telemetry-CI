a866782232e2bf0d1cd1dc4cb38dffa0fd02d8b3
jgalans <galansjavier@gmail.com>
2020-07-07 10:58:48 +0200

    Changing order of commands

--- test.sh
+++ test.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-#Ping desde host1 a host2
-sudo docker exec -it host1 ping 192.168.2.2 -c10
-
 #Ping desde host1 a switch1
 sudo docker exec -it host1 ping 192.168.1.1 -c50
 
 #Ping desde switch1 a host1
 sudo docker exec -it switch1 ping 192.168.1.2 -c50
 
-#Ping desde host2 a host1
-sudo docker exec -it host2 ping 192.168.1.2 -c10
+#Ping desde host1 a host2
+sudo docker exec -it host1 ping 192.168.2.2 -c50
 
 #Ping desde host2 a switch2
 sudo docker exec -it host2 ping 192.168.2.1 -c50
@@ -18,7 +15,10 @@ sudo docker exec -it host2 ping 192.168.2.1 -c50
 #Ping desde switch2 a host2
 sudo docker exec -it switch2 ping 192.168.2.2 -c50
 
-sleep 20 
+#Ping desde host2 a host1
+sudo docker exec -it host2 ping 192.168.1.2 -c50
+
+sleep 10 
 
 #Monitorización switch1
 sudo echo "Paquetes recibidos por Ethernet 1 en switch1"
