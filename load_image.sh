a986131d0baa5cc3f77d179b81da0e4670985dfb
Jandrov <jandrov.97@gmail.com>
2020-12-20 21:10:31 +0100

    Arreglar if en bash

--- load_image.sh
+++ load_image.sh
@@ -1,5 +1,5 @@
 #wget https://sonic-jenkins.westus2.cloudapp.azure.com/job/p4/job/buildimage-p4-all/543/artifact/target/docker-sonic-p4.gz
-if [ ! -f docker-sonic-p4.gz ] then
+if [ ! -f docker-sonic-p4.gz ]; then
 	wget https://sonic-jenkins.westus2.cloudapp.azure.com/job/p4/job/buildimage-p4-all/lastStableBuild/artifact/target/docker-sonic-p4.gz
 fi
 
