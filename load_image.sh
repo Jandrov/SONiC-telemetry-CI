6852d8241f07fb3285142159c25ce24186c5f91f
Alejandro Sánchez Sanz <jandrov.97@gmail.com>
2020-12-21 00:59:44 +0100

    Primeras pruebas de travis (#1)
    - Actualizar algunos ficheros

--- load_image.sh
+++ load_image.sh
@@ -1,9 +1,12 @@
 #wget https://sonic-jenkins.westus2.cloudapp.azure.com/job/p4/job/buildimage-p4-all/543/artifact/target/docker-sonic-p4.gz
-wget https://sonic-jenkins.westus2.cloudapp.azure.com/job/p4/job/buildimage-p4-all/lastStableBuild/artifact/target/docker-sonic-p4.gz
+if [ ! -f docker-sonic-p4.gz ]; then
+	wget https://sonic-jenkins.westus2.cloudapp.azure.com/job/p4/job/buildimage-p4-all/lastStableBuild/artifact/target/docker-sonic-p4.gz
+fi
 
 sudo docker load < docker-sonic-p4.gz
+sudo docker pull ubuntu:14.04
 
 #Contruimos las imagenes atendiendo al Dockerfile
-sudo docker build -f Dockerfile_sonic . 
+sudo docker build -f Dockerfile_sonic .
 sudo docker build -f Dockerfile_golang1 . -t gnmi_client
 sudo docker images
